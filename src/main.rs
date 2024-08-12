5ffe3ab7e80a187893983b7ecf1913b1ad757ec8
drtomflint <drtomflint@gmail.com>
2024-08-12 16:24:32 -0600

    Fix deploy to copy html to RPi

--- src/main.rs
+++ src/main.rs
@@ -20,18 +20,19 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("./src/index.html").unwrap();
-        let length = contents.len();
-
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        println!("request index: {}", request_line);
+        ("HTTP/1.1 200 OK", "./src/index.html")
     } else {
-        // some other request
-        println!("some other request {}", request_line);
-    }
+        println!("request unknown: {}", request_line);
+        ("HTTP/1.1 404 NOT FOUND", "./src/error.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
