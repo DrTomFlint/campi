039177f2d8dac967fd22f759936710cefe82c425
drtomflint <drtomflint@gmail.com>
2024-10-30 13:51:07 -0600

    Serve a static image file

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::{
 use campi::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:49000").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:9009").unwrap();
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
@@ -23,20 +23,60 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+    // let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+    //     println!("request index: {}", request_line);
+    //     let contents = fs::read_to_string(filename).unwrap();
+    //     ("HTTP/1.1 200 OK", "./src/index.html")
+    // } else if request_line == "GET /test HTTP/1.1" {
+    //     println!("request test: {}", request_line);
+    //     let contents = fs::read_to_string(filename).unwrap();
+    //     ("HTTP/1.1 200 OK", "./src/test.html")
+    // } else if request_line == "GET /src/d5.png HTTP/1.1" {
+    //     println!("request test: {}", request_line);
+    //     let contents = fs::read(filename).unwrap();
+    //     ("HTTP/1.1 200 OK", "./src/d5.png")
+    // } else {
+    //     println!("request unknown: {}", request_line);
+    //     let contents = fs::read_to_string(filename).unwrap();
+    //     ("HTTP/1.1 404 NOT FOUND", "./src/error.html")
+    // };
+
+    if request_line == "GET / HTTP/1.1" {
+        // index page
         println!("request index: {}", request_line);
-        ("HTTP/1.1 200 OK", "./src/index.html")
+        let contents = fs::read_to_string("./src/index.html").unwrap();
+        let status_line = "HTTP/1.1 200 OK";
+        let length = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+    } else if request_line == "GET /test HTTP/1.1" {
+        // test page
+        println!("request test: {}", request_line);
+        let contents = fs::read_to_string("./src/test.html").unwrap();
+        let status_line = "HTTP/1.1 200 OK";
+        let length = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+    } else if request_line == "GET /src/d5.png HTTP/1.1" {
+        // static image from file
+        println!("request test: {}", request_line);
+        let contents = fs::read("./src/d5.png").unwrap();
+        let status_line = "HTTP/1.1 200 OK";
+        let length = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n");
+        stream.write(response.as_bytes()).unwrap();
+        stream.write_all(&contents).unwrap();
+
     } else {
+        // error page
         println!("request unknown: {}", request_line);
-        ("HTTP/1.1 404 NOT FOUND", "./src/error.html")
+        let contents = fs::read_to_string("./src/error.html").unwrap();
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let length = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
 }
 
